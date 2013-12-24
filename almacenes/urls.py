c7449f6e64614e908cc04cafd3ce1477c3a512d6
gabriel <gabofer82@gmail.com>
2013-12-24 00:02:54 -0200

    Primer intento de mapeo de urls.

--- almacenes/urls.py
+++ almacenes/urls.py
@@ -5,6 +5,22 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    #USUARIOS
+    url(r'^usuarios/alta/$', 'usuarios.views.alta', name='alta_usuario'),
+    url(r'^usuarios/edicion/$', 'usuarios.views.edicion', name='edicion_usuario'),
+    url(r'^usuarios/baja/$', 'usuarios.views.baja', name='baja_usuario'),
+    #PRODUCTOS
+    url(r'^productos/alta/$', 'productos.views.alta', name='alta_producto'),
+    url(r'^productos/edicion/$', 'productos.views.edicion', name='edicion_producto'),
+    url(r'^productos/baja/$', 'productos.views.baja', name='baja_producto'),
+    #DEPOSITOS
+    url(r'^depositos/alta/$', 'depositos.views.alta', name='alta_deposito'),
+    url(r'^depositos/edicion/$', 'depositos.views.edicion', name='edicion_deposito'),
+    url(r'^depositos/baja/$', 'depositos.views.baja', name='baja_deposito'),
+    #REPORTES
+    url(r'^reportes/listado-de-usuarios/$', 'usuarios.views.listadoDeUsuarios', name='listado_usuarios'),
+    url(r'^reportes/listado-de-productos/$', 'usuarios.views.listadoDeProductos', name='listado_productos'),
+    url(r'^reportes/listado-de-depositos/$', 'usuarios.views.listadoDeDepositos', name='listado_depositos'),
     # Examples:
     # url(r'^$', 'almacenes.views.home', name='home'),
     # url(r'^almacenes/', include('almacenes.foo.urls')),
