b0a9fca3d361ab0ea80303eb35fc9246631772e4
gabriel <gabofer82@gmail.com>
2013-12-26 13:10:23 -0200

    Creado el modelo de proveedores

--- apps/proveedores/models.py
+++ apps/proveedores/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=24)
+    telefono1 = models.CharField(max_length=8)
+    telefono2 = models.CharField(max_length=8, null=True)
+    telefono3 = models.CharField(max_length=8, null=True)
+    celular = models.CharField(max_length=9, null=True)
+    domicilio = models.CharField(max_length=24, null=True)
